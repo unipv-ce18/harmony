1547313a09c78eb3b52fbdaabafbb284bd2d8441
Luca Zanussi <luca.z@outlook.com>
2019-10-25 23:38:24 +0200

    Initialize frontend toolchain

---
+++ webapp/webpack.common.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const APPLICATION_NAME = 'Harmony';
+
+module.exports = {
+
+  entry: './src/main.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?|png|jpg|mp4)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[hash].[ext]'
+        }
+      },
+      {
+        test: /\.m?js$/,
+        //exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-syntax-jsx',
+              "@babel/plugin-transform-property-mutators",
+              ["@babel/plugin-proposal-class-properties", {"loose": true}],
+              ['@babel/plugin-transform-react-jsx', {'pragma': 'h', 'pragmaFrag': 'Fragment'}]
+            ]
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      APP_NAME: JSON.stringify(APPLICATION_NAME)
+    }),
+    new HtmlPlugin({
+      title: APPLICATION_NAME,
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        preserveLineBreaks: false
+      }
+    })
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+
+};
---
+++ webapp/webpack.dev.js
@@ -0,0 +1,38 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+module.exports = merge(require('./webpack.common'), {
+
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {modules: true}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {plugins: [require('postcss-preset-env')]}
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: false,
+    hot: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+
+});
