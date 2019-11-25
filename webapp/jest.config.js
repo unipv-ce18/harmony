f04f474fc85d4f9e3eb154238e43771bce2e3a7e
Luca Zanussi <luca.z@outlook.com>
2019-11-25 16:25:39 +0100

    Prepare for CI

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(sa|sc|c)ss$": "identity-obj-proxy",
     "\\.(ttf|otf|eot|svg|woff(2)?|png|jpg|mp4)$": "<rootDir>/test/__mocks__/fileMock.js"
-  }
+  },
+  reporters: [ 'default', 'jest-junit' ]
 };
