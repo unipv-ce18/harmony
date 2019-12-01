3870465e840b7ad288e9efbd333df05519766b3d
luca <rockthisway05@gmail.com>
2019-12-01 12:24:52 +0100

    added CORS support on all api routes

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 import security
 from database import Database
