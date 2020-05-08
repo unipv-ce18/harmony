1b781486ea5da6bba964a3d0ef208fc4a4cb39cc
Andrea Rozza <andre.sixx.94@gmail.com>
2020-05-08 15:53:55 +0200

    port 80 apiserver

--- backend/apiserver/__main__.py
+++ backend/apiserver/__main__.py
@@ -6,6 +6,6 @@ eventlet.monkey_patch()
 from . import create_app, socketio
 
 
-# Start the application using the Flask's builtin werkzeug dev server (execute from backend/ $ python -m apiserver)
+# Start the application using the Flask's builtin werkzeug dev server (execute from backend/ $ sudo python -m apiserver)
 app = create_app(os.environ.get('FLASK_CONFIG', 'development'))
-socketio.run(app, host='127.0.0.1', port=5000)
+socketio.run(app, host='127.0.0.1', port=80)
