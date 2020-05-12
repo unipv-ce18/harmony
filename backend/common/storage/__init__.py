0a9ba0870e33f232b162e3ca797abc728d425914
Luca Zanussi <luca.z@outlook.com>
2020-05-12 17:34:53 +0200

    Set compressed songs bucket policy on creation

--- backend/common/storage/__init__.py
+++ backend/common/storage/__init__.py
@@ -2,6 +2,28 @@ from ..backend_config import BackendConfig
 from .storage import Storage
 
 
+def _make_public_bucket_policy(bucket_name):
+    import json
+    return json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": {"AWS": "*"},
+                "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
+                "Resource": f"arn:aws:s3:::{bucket_name}"
+            },
+            {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": {"AWS": "*"},
+                "Action": "s3:GetObject",
+                "Resource": f"arn:aws:s3:::{bucket_name}/*"
+            }
+        ]})
+
+
 def connect_storage(config: BackendConfig):
     import minio
     return minio.Minio(config.STORAGE_ENDPOINT,
@@ -13,9 +35,13 @@ def connect_storage(config: BackendConfig):
 def get_storage_interface(config: BackendConfig):
     st = Storage(connect_storage(config))
 
-    for bucket in [config.STORAGE_BUCKET_REFERENCE, config.STORAGE_BUCKET_TRANSCODED]:
-        if not st.check_bucket_exist(bucket):
-            st.create_bucket(bucket)
+    if not st.check_bucket_exist(config.STORAGE_BUCKET_REFERENCE):
+        st.create_bucket(config.STORAGE_BUCKET_REFERENCE)
+
+    if not st.check_bucket_exist(config.STORAGE_BUCKET_TRANSCODED):
+        st.create_bucket(config.STORAGE_BUCKET_TRANSCODED)
+        st.minio_client.set_bucket_policy(config.STORAGE_BUCKET_TRANSCODED,
+                                          _make_public_bucket_policy(config.STORAGE_BUCKET_TRANSCODED))
 
     return st
 
