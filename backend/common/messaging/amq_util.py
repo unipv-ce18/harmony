8d33b20233a9c30dab86f53b5ad6ff95ed79cc32
Luca Zanussi <luca.z@outlook.com>
2020-05-23 18:13:54 +0200

    Increase queue connection attempt count

--- backend/common/messaging/amq_util.py
+++ backend/common/messaging/amq_util.py
@@ -13,7 +13,7 @@ def amq_connect_blocking(config: BackendConfig):
         host=config.MESSAGING_HOST,
         port=config.MESSAGING_PORT,
         credentials=pika.PlainCredentials(config.MESSAGING_USERNAME, config.MESSAGING_PASSWORD),
-        connection_attempts=2, retry_delay=12    # cause Rabbit is slow to start (docker-compose), seriously 12s
+        connection_attempts=3, retry_delay=12    # cause Rabbit is slow to start (docker-compose), seriously 12s
     )
     return pika.BlockingConnection(conn_params)
 
