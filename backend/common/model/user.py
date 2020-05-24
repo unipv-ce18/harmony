60cf1d0022b66c4ac6cd056ff1bdbe9253faa417
A7F <rockthisway05@gmail.com>
2020-05-24 19:28:41 +0200

    bump up requirements, fix user model

--- backend/common/model/user.py
+++ backend/common/model/user.py
@@ -2,37 +2,37 @@ from collections import namedtuple
 
 _user_tuple = namedtuple('User', [
     'id',
+    'type',
+    'tier',
     'first_name',
     'last_name',
     'username',
     'email',
     'password',
     'bio',
+    'location',
     'avatar_url',
     'stats',
     'prefs'
 ])
 
 
-class User:
-    def __init__(self, user=None):
-        if user is None:
-            user = {}
-        if 'id' in user:
-            user['id'] = str(user['id'])
-        for i in _user_tuple._fields:
-            if i not in user:
-                user[i] = None
-        self.data = _user_tuple(**user)
+class User(_user_tuple):
 
     def __getattr__(self, item):
         return getattr(self.data, item)
 
-    def __repr__(self):
+    def __str__(self):
         user = ''
-        for k, v in self.data._asdict().items():
+        for k, v in self._asdict().items():
             user += f'{k}: {v}\n'
         return user
 
+    def __repr__(self):
+        user = ''
+        for k, v in self._asdict().items():
+            user += f'\n\t{k}: {v}\n'
+        return user
+
     def to_dict(self):
-        return dict(self.data._asdict())
+        return dict(self._asdict())
