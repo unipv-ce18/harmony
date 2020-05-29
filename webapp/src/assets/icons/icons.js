536fe92b176725de689b4d545176a05245b35fb2
Luca Zanussi <luca.z@outlook.com>
2020-05-29 23:49:32 +0200

    Composition for icons

--- webapp/src/assets/icons/icons.js
+++ webapp/src/assets/icons/icons.js
@@ -1,9 +1,9 @@
-function makeIcon(mod, props) {
+function Icon({mod, ...props}) {
   const {viewBox, url} = mod.default;
   const use = `<use xlink:href="${url}"/>`
   return (<svg {...props} viewBox={viewBox} dangerouslySetInnerHTML={{__html: use}}/>)
 }
 
-export const IconMusic = (props) => makeIcon(require('./music_note-24px.svg'), props);
-export const IconPlaylist = (props) => makeIcon(require('./library_books-24px.svg'), props);
-export const IconEqualizer = (props) => makeIcon(require('./equalizer-24px.svg'), props);
+export const IconMusic = (props) => <Icon mod={require('./music_note-24px.svg')} {...props}/>;
+export const IconPlaylist = (props) => <Icon mod={require('./library_books-24px.svg')} {...props}/>;
+export const IconEqualizer = (props) => <Icon mod={require('./equalizer-24px.svg')} {...props}/>;
