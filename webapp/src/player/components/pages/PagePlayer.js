390a0e0396ca6a389bb962e66427e642dbbc80a2
Luca Zanussi <luca.z@outlook.com>
2020-06-11 01:13:57 +0200

    Hacky font size calculations for PagePlayer

--- webapp/src/player/components/pages/PagePlayer.js
+++ webapp/src/player/components/pages/PagePlayer.js
@@ -3,13 +3,29 @@ import {Component, createRef} from 'preact';
 import {IconPlay, IconTrackNext, IconTrackPrev, IconTrackRepeat, IconTrackShuffle} from '../../../assets/icons/icons';
 import IconButton from '../IconButton';
 import Seekbar from '../Seekbar';
+import {getBoxFontSize, predictArtistFontSize, predictSongDataDomSize, predictTitleFontSize} from './playerPageMetrics';
+import {getExpandedSize} from '../playerUiPrefs';
 
 import style from './PagePlayer.scss';
 
+const TRANSITION_LEN = parseInt(style.playerTransitionLen);
+
 class PagePlayer extends Component {
 
-  titleLabelRef = createRef();
-  artistLabelRef = createRef();
+  #refs = {
+    songData: createRef(),  // to bind its children to ResizeObserver
+    titleLabel: createRef(),  // to calculate their FLIP size before leaving
+    artistLabel: createRef()
+  }
+
+  #resizeObserver = new ResizeObserver(els => els.forEach(e => {
+    // e.contentBoxSize only implemented by Firefox
+    const size = getBoxFontSize(e.contentRect);
+    e.target.style.fontSize = size + 'px';
+  }));
+
+  // Will need these pre-calculated before expansion to implement FLIP animations properly
+  #initialFontSize = null;
 
   componentWillEnter(done) {
     // Override default carousel animation
@@ -18,25 +34,58 @@ class PagePlayer extends Component {
   }
 
   componentWillLeave(done) {
-    this.titleLabelRef.current.saveCurrentLocation();
-    this.artistLabelRef.current.saveCurrentLocation();
+    // Save these before leaving for smooth transition to other pages
+    this.#refs.titleLabel.current.saveCurrentLocation();
+    this.#refs.artistLabel.current.saveCurrentLocation();
+
     done();
     return true;
   }
 
+  componentWillUpdate(nextProps, nextState, nextContext) {
+    // Invalidate font sizes in case we resized the player before collapsing
+    if (nextProps.expanded === false) this.#initialFontSize = null;
+  }
+
+  componentDidUpdate(previousProps, previousState, snapshot) {
+    if (this.props.expanded) {
+      // Bind resize observer only at animation end
+      setTimeout(() => {
+        const songData = this.#refs.songData.current;
+        if (songData) {
+          Array.from(songData.children)
+            .slice(0, 3)
+            .forEach(e => this.#resizeObserver.observe(e));
+        }
+      }, TRANSITION_LEN);
+    }
+  }
+
   render({expanded, flipCtx: Flip}, context) {
+    if (expanded && this.#initialFontSize == null) {
+      // Recalculate initial font sizes if needed
+      const sdSize = predictSongDataDomSize(getExpandedSize());
+      this.#initialFontSize = {title: predictTitleFontSize(sdSize), artist: predictArtistFontSize(sdSize)};
+    }
+
     return (
       <div class={style.pagePlayer}>
         <img src={require('../../../assets/albumart_default.jpg')}/>
         {expanded &&
-        <div class={style.songData}>
-          <Flip.Node ref={this.titleLabelRef} group="page-player" tag="track-title" scale>
-            <div>Best Song</div>
-          </Flip.Node>
-          <div>Da Album</div>
-          <Flip.Node ref={this.artistLabelRef} group="page-player" tag="track-artist" scale>
-            <div>A Fancy Artist</div>
-          </Flip.Node>
+        <div ref={this.#refs.songData} class={style.songData}>
+          <div style={{fontSize: this.#initialFontSize.title}}>
+            <Flip.Node ref={this.#refs.titleLabel} group="page-player" tag="track-title" scale>
+              <div>Best Song</div>
+            </Flip.Node>
+          </div>
+          <div>
+            <div>Da Album</div>
+          </div>
+          <div style={{fontSize: this.#initialFontSize.artist}}>
+            <Flip.Node ref={this.#refs.artistLabel} group="page-player" tag="track-artist" scale>
+              <div>A Fancy Artist</div>
+            </Flip.Node>
+          </div>
           <div class={style.seekbar}>
             <span>0.00</span>
             <Seekbar/>
