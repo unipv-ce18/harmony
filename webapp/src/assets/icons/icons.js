1260cd80b761007ba060edccfedf291da8a9f59f
Luca Zanussi <luca.z@outlook.com>
2020-06-10 11:49:07 +0200

    Add more icons

--- webapp/src/assets/icons/icons.js
+++ webapp/src/assets/icons/icons.js
@@ -4,9 +4,17 @@ function Icon({mod, ...props}) {
   return (<svg {...props} viewBox={viewBox} dangerouslySetInnerHTML={{__html: use}}/>)
 }
 
+// Player tabs
 export const IconMusic = (props) => <Icon mod={require('./music_note-24px.svg')} {...props}/>;
 export const IconPlaylist = (props) => <Icon mod={require('./library_books-24px.svg')} {...props}/>;
 export const IconEqualizer = (props) => <Icon mod={require('./equalizer-24px.svg')} {...props}/>;
 
+// Player generic
 export const IconPlay = (props) => <Icon mod={require('./play_arrow-24px.svg')} {...props}/>;
 export const IconPause = (props) => <Icon mod={require('./pause-24px.svg')} {...props}/>;
+
+// Player main page
+export const IconTrackNext = (props) => <Icon mod={require('./fast_forward-24px.svg')} {...props}/>;
+export const IconTrackPrev = (props) => <Icon mod={require('./fast_rewind-24px.svg')} {...props}/>;
+export const IconTrackRepeat = (props) => <Icon mod={require('./repeat-24px.svg')} {...props}/>;
+export const IconTrackShuffle = (props) => <Icon mod={require('./shuffle-24px.svg')} {...props}/>;
