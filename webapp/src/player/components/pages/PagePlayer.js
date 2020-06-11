ef0dddbfd2e5e2b980c94cab7ac5b1c0a2f105a5
Luca Zanussi <luca.z@outlook.com>
2020-06-11 12:51:29 +0200

    PagePlayer switch transitions

--- webapp/src/player/components/pages/PagePlayer.js
+++ webapp/src/player/components/pages/PagePlayer.js
@@ -1,5 +1,6 @@
 import {Component, createRef} from 'preact';
 
+import {classList} from '../../../core/utils';
 import {IconPlay, IconTrackNext, IconTrackPrev, IconTrackRepeat, IconTrackShuffle} from '../../../assets/icons/icons';
 import IconButton from '../IconButton';
 import Seekbar from '../Seekbar';
@@ -27,8 +28,40 @@ class PagePlayer extends Component {
   // Will need these pre-calculated before expansion to implement FLIP animations properly
   #initialFontSize = null;
 
+  state = {
+    visible: false
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    // Invalidate font sizes in case we resized the player before collapsing
+    if (nextProps.expanded === false) this.#initialFontSize = null;
+
+    // Detect expand/collapse and set visible accordingly
+    if (nextProps.expanded !== this.props.expanded)
+      this.setState({visible: nextProps.expanded});
+  }
+
+  componentDidUpdate(previousProps, previousState, snapshot) {
+    // If visibility changes, bind/unbind resize observer over title, album, artist labels
+    if (this.state.visible !== previousState.visible) {
+      const songData = this.#refs.songData.current;
+      if (this.state.visible) {
+        // Bind resize observer only at animation end
+        setTimeout(() => {
+          Array.from(songData.children).slice(0, 3)
+            .forEach(e => this.#resizeObserver.observe(e));
+        }, TRANSITION_LEN);
+      } else {
+        Array.from(songData.children).slice(0, 3)
+          .forEach(e => this.#resizeObserver.unobserve(e));
+      }
+    }
+  }
+
   componentWillEnter(done) {
     // Override default carousel animation
+    this.setState({visible: true});
+
     done();
     return true;
   }
@@ -37,31 +70,13 @@ class PagePlayer extends Component {
     // Save these before leaving for smooth transition to other pages
     this.#refs.titleLabel.current.saveCurrentLocation();
     this.#refs.artistLabel.current.saveCurrentLocation();
+    this.setState({visible: false});
 
-    done();
+    setTimeout(done, TRANSITION_LEN);
     return true;
   }
 
-  componentWillUpdate(nextProps, nextState, nextContext) {
-    // Invalidate font sizes in case we resized the player before collapsing
-    if (nextProps.expanded === false) this.#initialFontSize = null;
-  }
-
-  componentDidUpdate(previousProps, previousState, snapshot) {
-    if (this.props.expanded) {
-      // Bind resize observer only at animation end
-      setTimeout(() => {
-        const songData = this.#refs.songData.current;
-        if (songData) {
-          Array.from(songData.children)
-            .slice(0, 3)
-            .forEach(e => this.#resizeObserver.observe(e));
-        }
-      }, TRANSITION_LEN);
-    }
-  }
-
-  render({expanded, flipCtx: Flip}, context) {
+  render({expanded, flipCtx: Flip}, {visible}) {
     if (expanded && this.#initialFontSize == null) {
       // Recalculate initial font sizes if needed
       const sdSize = predictSongDataDomSize(getExpandedSize());
@@ -69,28 +84,44 @@ class PagePlayer extends Component {
     }
 
     return (
-      <div class={style.pagePlayer}>
-        <img src={require('../../../assets/albumart_default.jpg')}/>
-        {expanded &&
+      <div class={classList(style.pagePlayer, visible && style.visible)}>
+
+        {/* Album art */}
+        <img src={require('../../../assets/albumart_default.jpg')} alt=""/>
+
         <div ref={this.#refs.songData} class={style.songData}>
-          <div style={{fontSize: this.#initialFontSize.title}}>
+
+          {/* Song title */}
+          <div style={this.#initialFontSize && {fontSize: this.#initialFontSize.title}}>
+            {visible &&
             <Flip.Node ref={this.#refs.titleLabel} group="page-player" tag="track-title" scale>
               <div>Best Song</div>
             </Flip.Node>
+            }
           </div>
+
+          {/* Release */}
           <div>
             <div>Da Album</div>
           </div>
-          <div style={{fontSize: this.#initialFontSize.artist}}>
+
+          {/* Artist  */}
+          <div style={this.#initialFontSize && {fontSize: this.#initialFontSize.artist}}>
+            {visible &&
             <Flip.Node ref={this.#refs.artistLabel} group="page-player" tag="track-artist" scale>
               <div>A Fancy Artist</div>
             </Flip.Node>
+            }
           </div>
+
+          {/* Seekbar */}
           <div class={style.seekbar}>
             <span>0.00</span>
-            <Seekbar/>
+            <Seekbar enabled={visible}/>
             <span>4.05</span>
           </div>
+
+          {/* Player controls */}
           <div class={style.controls}>
             <div>
               <IconButton name="Repeat" size={16} icon={IconTrackRepeat}/>
@@ -102,8 +133,8 @@ class PagePlayer extends Component {
               <IconButton name="Shuffle" size={16} icon={IconTrackShuffle}/>
             </div>
           </div>
+
         </div>
-        }
       </div>
     );
   }
