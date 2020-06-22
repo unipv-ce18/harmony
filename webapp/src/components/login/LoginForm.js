e06e3555a3bea03464c06107554327cd6da2f817
Luca Zanussi <luca.z@outlook.com>
2020-06-22 20:52:25 +0200

    Animate between login and signup form

--- webapp/src/components/login/LoginForm.js
+++ webapp/src/components/login/LoginForm.js
@@ -1,11 +1,15 @@
 import {Component} from 'preact';
 import PropTypes from 'prop-types';
+import TransitionGroup from 'preact-transition-group';
 
 import {session} from '../../Harmony';
 import {FieldType, validateEmail, validatePassword, validateUsername, ValidationError} from './validation';
+import {ApiError, execRegistration} from '../../core/apiCalls';
 
 import style from './LoginForm.scss';
-import {ApiError, execRegistration} from '../../core/apiCalls';
+
+const TRANSITION_LEN = parseInt(style.formTransitionLen);
+const FIELD_MARGIN_REGISTRATION = parseInt(style.fieldMarginRegistration)
 
 class LoginForm extends Component {
 
@@ -75,14 +79,12 @@ class LoginForm extends Component {
     }
   }
 
-  render({registration}) {
-    const FormField = this.FormField;
-
+  render({registration}, {error}) {
     const vars = registration ? {
       formClass: style.registrationForm,
       submitHandler: this.handleSubmitRegister,
       submitLabel: 'Sign Up',
-      switchHint: (<p className={style.regLink}>Are you already registered? <a href="/login">Login</a> now</p>)
+      switchHint: (<p className={style.regLink}>Already have an account? <a href="/login">Login</a> now</p>)
     } : {
       formClass: style.loginForm,
       submitHandler: this.handleSubmitLogin,
@@ -90,17 +92,26 @@ class LoginForm extends Component {
       switchHint: (<p className={style.regLink}>Not yet registered? <a href="/signup">Sign Up</a> now</p>)
     };
 
+    const commonProps = {error, onChange: this.handleChange, onFocus: this.handleFocus}
+
     return (
       <div className={style.formWrapper}>
         <form className={`${style.form} ${vars.formClass}`} onSubmit={vars.submitHandler}>
+          <TransitionGroup component="div">
+            {registration && (
+              <FormField key="e" fieldType={FieldType.EMAIL} name="fieldEmail" placeholder="Email" {...commonProps}/>
+            )}
+            <FormField key="u" fieldType={FieldType.USERNAME} name="fieldUsername" placeholder="Username" {...commonProps}/>
+            <FormField key="p1" fieldType={FieldType.PASSWORD_1} name="fieldPassword1" placeholder="Password" {...commonProps}/>
+            {registration && (
+              <FormField key="p2" fieldType={FieldType.PASSWORD_2} name="fieldPassword2" placeholder="Repeat Password" {...commonProps}/>
+            )}
+          </TransitionGroup>
           <div>
-            {registration && <FormField fieldType={FieldType.EMAIL} name="fieldEmail" placeholder="Email"/>}
-            <FormField fieldType={FieldType.USERNAME} name="fieldUsername" placeholder="Username"/>
-            <FormField fieldType={FieldType.PASSWORD_1} name="fieldPassword1" placeholder="Password"/>
-            {registration && <FormField fieldType={FieldType.PASSWORD_2} name="fieldPassword2" placeholder="Repeat Password"/>}
-          </div>
-          <div>
-            {!registration && [<input type="checkbox"/>, <label>Remember me</label>]}
+            <div class={style.rememberMe}>
+              <input type="checkbox"/>
+              <label>Remember me</label>
+            </div>
             <input type="submit" value={vars.submitLabel}/>
           </div>
         </form>
@@ -123,22 +134,38 @@ class LoginForm extends Component {
     }
   }
 
-  FormField = ({fieldType, placeholder, name, ...props}) => {
-    const ERROR_STYLE = `border: 1px solid #bf0000`;
+}
 
-    const errorType = this.state.error && this.state.error.type;
+class FormField extends Component {
+
+  static ERROR_STYLE = `border: 1px solid #bf0000`;
+  static ANIMATION_OPTIONS = {duration: TRANSITION_LEN, easing: 'ease'};
+
+  componentWillEnter(done) {
+    const h = (this.base.children[0].clientHeight + FIELD_MARGIN_REGISTRATION) + 'px';
+    this.base.animate([{height: '0', opacity: '0'}, {height: h, opacity: '1'}], FormField.ANIMATION_OPTIONS).onfinish = done;
+  }
+
+  componentWillLeave(done) {
+    const h = this.base.clientHeight + 'px';
+    this.base.animate([{height: h, opacity: '1'}, {height: '0', opacity: '0'}], FormField.ANIMATION_OPTIONS).onfinish = done;
+  }
+
+  render({fieldType, placeholder, name, error, onChange, onFocus, ...props}) {
+
+    const errorType = error && error.type;
     const isPassword = fieldType === FieldType.PASSWORD_1 || fieldType === FieldType.PASSWORD_2;
 
     return (
-      <span>
+      <div class={style.fieldWrapper}>
         <input type={isPassword ? "password" : "text"} placeholder={placeholder} name={name}
-               onChange={this.handleChange} onFocus={this.handleFocus} {...props}
-               style={errorType === fieldType && ERROR_STYLE}/>
+               onChange={onChange} onFocus={onFocus} {...props}
+               style={errorType === fieldType && FormField.ERROR_STYLE}/>
         {errorType === fieldType && (
-          <div class={style.errorField}><span/>{this.state.error.message}</div>
+          <div class={style.errorField}><span/>{error.message}</div>
         )}
-    </span>
-    )
+      </div>
+    );
   }
 
 }
