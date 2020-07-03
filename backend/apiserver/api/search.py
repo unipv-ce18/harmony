ef0404de62d373e14824df704d08ae35c7c80489
Luca Zanussi <luca.z@outlook.com>
2020-07-03 17:51:03 +0200

    Update search API

--- backend/apiserver/api/search.py
+++ backend/apiserver/api/search.py
@@ -21,7 +21,7 @@ class Search(Resource):
     method_decorators = [security.jwt_required]
 
     def get(self):
-        """Search
+        """Search inside Harmony
         ---
         tags: [misc]
         parameters:
@@ -30,12 +30,13 @@ class Search(Resource):
             schema:
               type: string
             required: true
-            description: query
-            example: queens
+            description: The search query
+            example: Maximum Sexy Pigeon
           - in: query
             name: t
             schema:
               type: string
+              enum: ['any', 'artists', 'releases', 'songs', 'playlists']
             required: false
             description: Type of search
           - in: query
@@ -43,47 +44,51 @@ class Search(Resource):
             schema:
               type: int
             required: false
-            description: Start
+            description: Start index of the results
           - in: query
             name: c
             schema:
               type: int
             required: false
-            description: Count
+            description: Number of the results to return per category
         responses:
           200:
-            description: Successful search
+            description: Successful search for `any`, no results
             content:
               application/json:
                 example: {
-
+                  "artists": [],
+                  "releases": [],
+                  "songs": [],
+                  "playlists": []
                 }
+          400:
+            description: Unrecognized search type argument
+            content:
+              application/json:
+                example: {"message": 'Unknown search type: "foobar"'}
         """
+        _check_input = lambda x: x is not None and x > 0
+        _convert_result = lambda res: [r.to_dict() for r in res]
+
         data = _arg_parser_search.parse_args()
-        _check = lambda x : x is not None and x > 0
 
         query = data['q']
-        type = data['t'] or 'any'
-        start = data['s'] if _check(data['s']) else 0
-        count = data['c'] if _check(data['c']) else 50
+        search_type = data['t'] or 'any'
+        start = data['s'] if _check_input(data['s']) else 0
+        count = data['c'] if _check_input(data['c']) else 50
 
-        switch = {
-            'artists': db.search_artist(query, start, count),
-            'releases': db.search_release(query, start, count),
-            'songs': db.search_song(query, start, count),
-            'playlists': db.search_playlist(query, start, count)
-        }
+        result = {}
+        if search_type in ['any', 'artists']:
+            result['artists'] = _convert_result(db.search_artist(query, start, count))
+        if search_type in ['any', 'releases']:
+            result['releases'] = _convert_result(db.search_release(query, start, count))
+        if search_type in ['any', 'songs']:
+            result['songs'] = _convert_result(db.search_song(query, start, count))
+        if search_type in ['any', 'playlists']:
+            result['playlists'] = _convert_result(db.search_playlist(query, start, count))
 
-        result = {**{'any': switch}, **switch}.get(type)
+        if len(result) == 0:
+            return {'message': f'Unknown search type: "{search_type}"'}, HTTPStatus.BAD_REQUEST
 
-        if isinstance(result, list) and result:
-            return [res.to_dict() for res in result], HTTPStatus.OK
-
-        if isinstance(result, dict):
-            for k in result:
-                if result[k]:
-                    result[k] = [res.to_dict() for res in result[k]]
-            if not all(v == [] for v in result.values()):
-                return result, HTTPStatus.OK
-
-        return [], HTTPStatus.OK
+        return result, HTTPStatus.OK
