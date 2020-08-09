dc419394000538abbc74dafe58e7499b3b5a85c7
Luca Zanussi <luca.z@outlook.com>
2020-08-09 15:47:22 +0200

    Update user widget dropdown
    - Moved to a separate component;
    
    - Polished transition effect;
    
    - Now shows on mouse hover.

---
+++ webapp/src/header/UserWidget.js
@@ -0,0 +1,67 @@
+import {Component, createRef} from 'preact';
+
+import {classList} from '../core/utils';
+import {session} from '../Harmony';
+import {DEFAULT_USER_IMAGE_URL} from '../assets/defaults';
+
+import style from './UserWidget.scss';
+
+const LEAVE_TIMEOUT_MS = 300;
+
+class UserWidget extends Component {
+
+  constructor() {
+    super();
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  state = {
+    dropdown: false,
+    dropdownHeight: 0
+  }
+
+  #leaveTimeout = null;
+  #navRef = createRef();
+
+  componentDidMount() {
+    this.setState({dropdownHeight: this.#navRef.current.clientHeight + 'px'});
+  }
+
+  render(props, {dropdown, dropdownHeight}) {
+    return (
+      <div className={classList(style.userWidget, dropdown && `drop-down`)} style={{'--dd-height': dropdownHeight}}
+           onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        <div>
+          <span>{session.getOwnData().username}</span>
+          <img src={DEFAULT_USER_IMAGE_URL} alt=""/>
+        </div>
+        <div>
+          <div ref={this.#navRef} onClick={() => this.setState({dropdown: false})}>
+          <a href="/user/me">My profile</a>
+          <a href="#" onClick={e => {e.preventDefault(); session.doLogout();}}>Log out</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onMouseEnter(e) {
+    if (this.#leaveTimeout != null) {
+      clearTimeout(this.#leaveTimeout);
+      this.#leaveTimeout = null;
+    } else {
+      this.setState({dropdown: true});
+    }
+  }
+
+  onMouseLeave(e) {
+    this.#leaveTimeout = setTimeout(() => {
+      this.setState({dropdown: false});
+      this.#leaveTimeout = null;
+    }, LEAVE_TIMEOUT_MS);
+  }
+
+}
+
+export default UserWidget;
