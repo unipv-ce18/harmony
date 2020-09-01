0ac7c31679c741315dcddb0041db8f8d4ee4fa83
Andrea Rozza <andre.sixx.94@gmail.com>
2020-09-01 21:31:12 +0200

    click on genre from artist page

--- webapp/src/components/artist/Tags.js
+++ webapp/src/components/artist/Tags.js
@@ -1,11 +1,19 @@
 import {Component} from 'preact';
+import {route} from 'preact-router';
+
 import styles from './ArtistPage.scss';
 
 class Tags extends Component {
+
+  clickGenre(genre, e) {
+     e.preventDefault();
+     route('/search/:genre=' + genre);
+  }
+
   render({list}) {
     return (
       <div class={styles.tagsList}>
-        {list.map(item => item!==null && <span class={styles.tag} key={item.id}>{item}</span>)}
+        {list.map(item => item!==null && <button onClick={this.clickGenre.bind(this, item)}><span class={styles.tag} key={item.id}>{item}</span></button>)}
       </div>);
   }
 }
