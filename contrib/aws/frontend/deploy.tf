dd856b1eac0aaf594e128c715741b2d9fcb8b844
Luca Zanussi <luca.z@outlook.com>
2020-10-15 18:16:23 +0200

    [TF] Prevent direct access to S3 deployment bucket

--- contrib/aws/frontend/deploy.tf
+++ contrib/aws/frontend/deploy.tf
@@ -28,19 +28,19 @@ resource "aws_s3_bucket_public_access_block" "b" {
 
 resource "aws_s3_bucket_policy" "b" {
   bucket = aws_s3_bucket.b.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "${aws_s3_bucket.b.arn}/*"
-    }
-  ]
+  policy = data.aws_iam_policy_document.bp.json
 }
-EOF
+
+data "aws_iam_policy_document" "bp" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+    principals {
+      type = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.webapp_s3.iam_arn]
+    }
+  }
 }
 
 
@@ -48,10 +48,19 @@ locals {
   s3_origin_id = "S3-hy-webapp"
 }
 
+# Used by CloudFront to access the webapp bucket
+resource "aws_cloudfront_origin_access_identity" "webapp_s3" {
+  comment = "Webapp deploy access"
+}
+
 resource "aws_cloudfront_distribution" "webapp" {
   origin {
     domain_name = aws_s3_bucket.b.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.webapp_s3.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
